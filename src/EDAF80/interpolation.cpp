78a2e7be7e547147c69cbf0b6cdd1ec386afd7de
Aidan-harrer <147786021+Aidan-harrer@users.noreply.github.com>
2024-09-25 16:26:34 +0200

    final interpolation.cpp

--- src/EDAF80/interpolation.cpp
+++ src/EDAF80/interpolation.cpp
@@ -3,8 +3,8 @@
 glm::vec3
 interpolation::evalLERP(glm::vec3 const& p0, glm::vec3 const& p1, float const x)
 {
-	//! \todo Implement this function
-	return glm::vec3();
+	glm::vec3 v = (1 - x) * p0 + x * p1;
+	return v;
 }
 
 glm::vec3
@@ -12,6 +12,12 @@ interpolation::evalCatmullRom(glm::vec3 const& p0, glm::vec3 const& p1,
                               glm::vec3 const& p2, glm::vec3 const& p3,
                               float const t, float const x)
 {
-	//! \todo Implement this function
-	return glm::vec3();
+	glm::vec4 m1 = glm::vec4( 1,x,x * x,x * x * x );
+	glm::mat4 m2 = glm::mat4({ 0,-t,2 * t,-t }, {1,0,t-3,2-t}, {0,t,3-2*t,t-2}, {0,0,-t,t});
+	glm::mat4x3 m3 = glm::transpose(glm::mat4x3( p0,p1,p2,p3 ));
+	glm::vec3 pos = m1 * m2 * m3;
+
+
+
+	return pos;
 }
