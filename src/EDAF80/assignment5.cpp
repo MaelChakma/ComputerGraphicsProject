726d583d85e3c5bcedfea3d5bb8963a6abdd7fcf
Aidan Harrer <aidan.harrer@du.edu>
2024-10-14 11:45:27 +0200

    added a sun

--- src/EDAF80/assignment5.cpp
+++ src/EDAF80/assignment5.cpp
@@ -1,27 +1,30 @@
 #include "assignment5.hpp"
+#include "parametric_shapes.cpp"
 
 #include "config.hpp"
 #include "core/Bonobo.h"
 #include "core/FPSCamera.h"
 #include "core/helpers.hpp"
+#include "core/node.hpp"
 #include "core/ShaderProgramManager.hpp"
 
+#include <glm/gtc/type_ptr.hpp>
 #include <imgui.h>
 #include <tinyfiledialogs.h>
 
 #include <clocale>
 #include <stdexcept>
 
-edaf80::Assignment5::Assignment5(WindowManager& windowManager) :
-	mCamera(0.5f * glm::half_pi<float>(),
-	        static_cast<float>(config::resolution_x) / static_cast<float>(config::resolution_y),
-	        0.01f, 1000.0f),
-	inputHandler(), mWindowManager(windowManager), window(nullptr)
+edaf80::Assignment5::Assignment5(WindowManager &windowManager) : mCamera(0.5f * glm::half_pi<float>(),
+																		 static_cast<float>(config::resolution_x) / static_cast<float>(config::resolution_y),
+																		 0.01f, 1000.0f),
+																 inputHandler(), mWindowManager(windowManager), window(nullptr)
 {
-	WindowManager::WindowDatum window_datum{ inputHandler, mCamera, config::resolution_x, config::resolution_y, 0, 0, 0, 0};
+	WindowManager::WindowDatum window_datum{inputHandler, mCamera, config::resolution_x, config::resolution_y, 0, 0, 0, 0};
 
 	window = mWindowManager.CreateGLFWWindow("EDAF80: Assignment 5", window_datum, config::msaa_rate);
-	if (window == nullptr) {
+	if (window == nullptr)
+	{
 		throw std::runtime_error("Failed to get a window: aborting!");
 	}
 
@@ -33,8 +36,7 @@ edaf80::Assignment5::~Assignment5()
 	bonobo::deinit();
 }
 
-void
-edaf80::Assignment5::run()
+void edaf80::Assignment5::run()
 {
 	// Set up the camera
 	mCamera.mWorld.SetTranslate(glm::vec3(0.0f, 0.0f, 6.0f));
@@ -45,14 +47,46 @@ edaf80::Assignment5::run()
 	ShaderProgramManager program_manager;
 	GLuint fallback_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Fallback",
-	                                         { { ShaderType::vertex, "common/fallback.vert" },
-	                                           { ShaderType::fragment, "common/fallback.frag" } },
-	                                         fallback_shader);
-	if (fallback_shader == 0u) {
+											 {{ShaderType::vertex, "common/fallback.vert"},
+											  {ShaderType::fragment, "common/fallback.frag"}},
+											 fallback_shader);
+	if (fallback_shader == 0u)
+	{
 		LogError("Failed to load fallback shader");
 		return;
 	}
 
+	GLuint skybox_shader = 0u;
+	program_manager.CreateAndRegisterProgram("Skybox",
+											 {{ShaderType::vertex, "EDAF80/skybox.vert"},
+											  {ShaderType::fragment, "EDAF80/skybox.frag"}},
+											 skybox_shader);
+
+	auto skybox_shape = parametric_shapes::createSphere(100.0f, 100u, 100u);
+	if (skybox_shape.vao == 0u)
+	{
+		LogError("Failed to retrieve the mesh for the skybox");
+		return;
+	}
+
+	auto light_position = glm::vec3(-2.0f, 4.0f, 2.0f);
+	auto const set_uniforms = [&light_position](GLuint program)
+	{
+		glUniform3fv(glGetUniformLocation(program, "light_position"), 1, glm::value_ptr(light_position));
+	};
+
+	GLuint cubemap = bonobo::loadTextureCubeMap(
+		config::resources_path("cubemaps/Space/right.png"),
+		config::resources_path("cubemaps/Space/left.png"),
+		config::resources_path("cubemaps/Space/top.png"),
+		config::resources_path("cubemaps/Space/bottom.png"),
+		config::resources_path("cubemaps/Space/front.png"),
+		config::resources_path("cubemaps/Space/back.png"));
+
+	Node skybox;
+	skybox.set_geometry(skybox_shape);
+	skybox.set_program(&skybox_shader, set_uniforms);
+	skybox.add_texture("cubemap", cubemap, GL_TEXTURE_CUBE_MAP);
 	//
 	// Todo: Insert the creation of other shader programs.
 	//       (Check how it was done in assignment 3.)
@@ -66,7 +100,6 @@ edaf80::Assignment5::run()
 	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 	glEnable(GL_DEPTH_TEST);
 
-
 	auto lastTime = std::chrono::high_resolution_clock::now();
 
 	bool show_logs = true;
@@ -76,25 +109,27 @@ edaf80::Assignment5::run()
 	float basis_thickness_scale = 1.0f;
 	float basis_length_scale = 1.0f;
 
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window))
+	{
 		auto const nowTime = std::chrono::high_resolution_clock::now();
 		auto const deltaTimeUs = std::chrono::duration_cast<std::chrono::microseconds>(nowTime - lastTime);
 		lastTime = nowTime;
 
-		auto& io = ImGui::GetIO();
+		auto &io = ImGui::GetIO();
 		inputHandler.SetUICapture(io.WantCaptureMouse, io.WantCaptureKeyboard);
 
 		glfwPollEvents();
 		inputHandler.Advance();
 		mCamera.Update(deltaTimeUs, inputHandler);
 
-		if (inputHandler.GetKeycodeState(GLFW_KEY_R) & JUST_PRESSED) {
+		if (inputHandler.GetKeycodeState(GLFW_KEY_R) & JUST_PRESSED)
+		{
 			shader_reload_failed = !program_manager.ReloadAllPrograms();
 			if (shader_reload_failed)
 				tinyfd_notifyPopup("Shader Program Reload Error",
-				                   "An error occurred while reloading shader programs; see the logs for details.\n"
-				                   "Rendering is suspended until the issue is solved. Once fixed, just reload the shaders again.",
-				                   "error");
+								   "An error occurred while reloading shader programs; see the logs for details.\n"
+								   "Rendering is suspended until the issue is solved. Once fixed, just reload the shaders again.",
+								   "error");
 		}
 		if (inputHandler.GetKeycodeState(GLFW_KEY_F3) & JUST_RELEASED)
 			show_logs = !show_logs;
@@ -102,7 +137,6 @@ edaf80::Assignment5::run()
 			show_gui = !show_gui;
 		if (inputHandler.GetKeycodeState(GLFW_KEY_F11) & JUST_RELEASED)
 			mWindowManager.ToggleFullscreenStatusForWindow(window);
-
 
 		// Retrieve the actual framebuffer size: for HiDPI monitors,
 		// you might end up with a framebuffer larger than what you
@@ -115,23 +149,18 @@ edaf80::Assignment5::run()
 		glfwGetFramebufferSize(window, &framebuffer_width, &framebuffer_height);
 		glViewport(0, 0, framebuffer_width, framebuffer_height);
 
-
 		//
 		// Todo: If you need to handle inputs, you can do it here
 		//
-
 
 		mWindowManager.NewImGuiFrame();
 
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-
-		if (!shader_reload_failed) {
-			//
-			// Todo: Render all your geometry here.
-			//
+		if (!shader_reload_failed)
+		{
+			skybox.render(mCamera.GetWorldToClipMatrix());
 		}
-
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
@@ -140,7 +169,8 @@ edaf80::Assignment5::run()
 		//       here
 		//
 		bool const opened = ImGui::Begin("Scene Controls", nullptr, ImGuiWindowFlags_None);
-		if (opened) {
+		if (opened)
+		{
 			ImGui::Checkbox("Show basis", &show_basis);
 			ImGui::SliderFloat("Basis thickness scale", &basis_thickness_scale, 0.0f, 100.0f);
 			ImGui::SliderFloat("Basis length scale", &basis_length_scale, 0.0f, 100.0f);
@@ -163,10 +193,13 @@ int main()
 
 	Bonobo framework;
 
-	try {
+	try
+	{
 		edaf80::Assignment5 assignment5(framework.GetWindowManager());
 		assignment5.run();
-	} catch (std::runtime_error const& e) {
+	}
+	catch (std::runtime_error const &e)
+	{
 		LogError(e.what());
 	}
 }
